3ac5f2c7d3b611279c9c2f79964340713a5c6abe
BlueTree-Na <106299019+BlueTree-Na@users.noreply.github.com>
2024-04-20 02:31:57 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int[] box, int n) {
         int answer = 1;
         
-        for (int i = 0; i < box.length; i++){
-            answer *= box[i] / n;
-        }
+        answer *= box[0] / n;
+        answer *= box[1] / n;
+        answer *= box[2] / n;
         
         return answer;
     }
