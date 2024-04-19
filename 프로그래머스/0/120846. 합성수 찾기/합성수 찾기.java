c1fe6742f84937754b8dde878f4f39c4e04f0b3d
BlueTree-Na <106299019+BlueTree-Na@users.noreply.github.com>
2024-04-20 02:47:17 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int cnt = 0;
+        
+        while(n != 0){
+            for(int i = 1; i <= n; i++){
+                if (n % i == 0){
+                    cnt++;
+                }
+            }
+            if (cnt >= 3){
+                answer++;
+            }
+            cnt = 0;
+            n--;
+        }
+        
+        
+        return answer;
+    }
+}
